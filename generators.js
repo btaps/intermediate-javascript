892428ea303c26bdc5152a467ba6de140228d050
Los Tapias <lostapias@Loss-MacBook-Pro.local>
2019-12-27 11:28:02 -0800

    fixed generators.js

--- generators.js
+++ generators.js
@@ -1,20 +1,66 @@
-let numbers = Infinity
 
 let generateFizzBuzz = function* () {
-  for(i = 0; i<= numbers; i++){
-    if(i % 5 === 0 && i % 3 === 0) yield 'FizzBuzz'
-    else if (i % 5 === 0) console.log('Buzz')
-    else if (i % 3 === 0) console.log('Fizz')
-    else console.log(i)
+  let i = 0  
+  while(true){
+    if(i % 5 === 0 && i % 3 === 0){
+      yield 'FizzBuzz'
+    }else if (i % 5 === 0){
+      yield('Buzz')
+    }else if (i % 3 === 0){
+      yield('Fizz')
+    }else {
+      yield i
     }
+   i++
+  }  
 }
 let generatorObject = generateFizzBuzz()
 
-console.log(generatorObject.next())
-console.log(generatorObject.next())
-console.log(generatorObject.next())
-console.log(generatorObject.next())
-console.log(generatorObject.next())
-console.log(generatorObject.next())
-console.log(generatorObject.next())
-console.log(generatorObject.next())
+console.log(generatorObject.next().value)
+console.log(generatorObject.next().value)
+console.log(generatorObject.next().value)
+console.log(generatorObject.next().value)
+console.log(generatorObject.next().value)
+console.log(generatorObject.next().value)
+console.log(generatorObject.next().value)
+console.log(generatorObject.next().value)
+console.log(generatorObject.next().value)
+console.log(generatorObject.next().value)
+console.log(generatorObject.next().value)
+console.log(generatorObject.next().value)
+console.log(generatorObject.next().value)
+console.log(generatorObject.next().value)
+console.log(generatorObject.next().value)
+console.log(generatorObject.next().value)
+console.log(generatorObject.next().value)
+console.log(generatorObject.next().value)
+console.log(generatorObject.next().value)
+console.log(generatorObject.next().value)
+console.log(generatorObject.next().value)
+console.log(generatorObject.next().value)
+console.log(generatorObject.next().value)
+console.log(generatorObject.next().value)
+console.log(generatorObject.next().value)
+console.log(generatorObject.next().value)
+console.log(generatorObject.next().value)
+console.log(generatorObject.next().value)
+console.log(generatorObject.next().value)
+console.log(generatorObject.next().value)
+console.log(generatorObject.next().value)
+console.log(generatorObject.next().value)
+console.log(generatorObject.next().value)
+console.log(generatorObject.next().value)
+console.log(generatorObject.next().value)
+console.log(generatorObject.next().value)
+console.log(generatorObject.next().value)
+console.log(generatorObject.next().value)
+console.log(generatorObject.next().value)
+console.log(generatorObject.next().value)
+console.log(generatorObject.next().value)
+console.log(generatorObject.next().value)
+console.log(generatorObject.next().value)
+console.log(generatorObject.next().value)
+console.log(generatorObject.next().value)
+console.log(generatorObject.next().value)
+console.log(generatorObject.next().value)
+console.log(generatorObject.next().value)
